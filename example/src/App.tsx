923660e5930a34ac3e29db13b13a87854fd072dc
Brandon Barreto Lozano <bbarreto@syntax.es>
2021-06-03 11:27:12 -0500

    prueba2

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
-import { Hello }  from 'components'
+import { Hello, Hello2 }  from 'components'
 import 'components/dist/index.css'  
 
 const App = () => {
   //return <ExampleComponent text="Create React Library Example 😄" />
-  return < Hello />
+  return <div>
+      < Hello2 />
+      < Hello />
+  </div> 
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react'
 import styles from './styles.module.css'
 
-// import MyNavBar from './components/Navbar'
+import MyNavBar from './components/Navbar'
 
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+// import AppBar from '@material-ui/core/AppBar';
+// import Toolbar from '@material-ui/core/Toolbar';
+// import Typography from '@material-ui/core/Typography';
+// import Button from '@material-ui/core/Button';
+// import IconButton from '@material-ui/core/IconButton';
+// import MenuIcon from '@material-ui/icons/Menu';
 // import moment from 'moment';
 
 import MyTextField from './TextField';
@@ -21,26 +21,15 @@ export const ExampleComponent = ({ text }: Props) => {
   return <div className={styles.test}>Example Component: {text}</div>
 }
 
-// export const Hello = () =>{
-//   return (
-//     <MyNavBar/>
-//   )
-// }
+export const Hello = () =>{
+  return (
+    <MyNavBar/>
+  )
+}
 
 export const Hello2 = () =>{
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <IconButton edge="start" color="inherit" aria-label="menu">
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" >
-          News
-        </Typography>
-        <Button color="inherit">Login</Button>
-        <Hello2 />
-      </Toolbar>
-    </AppBar>
+    <div>hola mundo</div>
   )
 }
 
